fc437382ad31f589eb88772db09f038ec5d91ac0
Кучеренко Алексаднр <vilsashka2007@gmail.com>
2025-10-07 12:23:17 +0300

    Update a2.py

--- lab2/a2.py
+++ lab2/a2.py
@@ -1,29 +1,10 @@
-
-try:
-    print(int('123е'))
-except Exception as e:
-    print(f"1. Ошибка: {type(e).__name__}: {e}")
-try:
-    print(int('91.4'))
-except Exception as e:
-    print(f"2. Ошибка: {type(e).__name__}: {e}")
-try:
-    print(int('7.1 + 4'))
-except Exception as e:
-    print(f"4. Ошибка: {type(e).__name__}: {e}")
-try:
-    print(int('4' - 2))
-except Exception as e:
-    print(f"5. Ошибка: {type(e).__name__}: {e}")
-try:
-    print(int('4 - 2'))
-except Exception as e:
-    print(f"6. Ошибка: {type(e).__name__}: {e}")
-try:
-    print(f"7. Результат: {int('42')}")
-except Exception as e:
-    print(f"7. Ошибка: {type(e).__name__}: {e}")
-try:
-    print(f"8. Результат: {int(-12.12)}")
-except Exception as e:
-    print(f"8. Ошибка: {type(e).__name__}: {e}")
+width = 17
+height = 12.0
+x = width //2
+y = width / 2.0
+z = height /3
+d = 1 + 2 * 5
+print(x)
+print(y)
+print(z)
+print(d)
