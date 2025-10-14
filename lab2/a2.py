9c74982eb586d89e86d1a63b1b3a18f8c16a5274
Кучеренко Алексаднр <vilsashka2007@gmail.com>
2025-10-14 12:06:35 +0300

    Update a2.py

--- lab2/a2.py
+++ lab2/a2.py
@@ -1,10 +1,16 @@
-width = 17
-height = 12.0
-x = width //2
-y = width / 2.0
-z = height /3
-d = 1 + 2 * 5
-print(x)
-print(y)
-print(z)
-print(d)
+int('123е')
+#Ошибка: ValueError: invalid literal for int() with base 10: '123е'
+int('91.4')
+#Ошибка: ValueError: invalid literal for int() with base 10: '91.4'
+int(524.345 ** 435345345311145345)
+#Ошибка: OverflowError: int too large to convert to float
+int('7.1 + 4')
+#Ошибка: ValueError: invalid literal for int() with base 10: '7.1 + 4'
+int('4' - 2)
+#Ошибка: TypeError: unsupported operand type(s) for -: 'str' and 'int'
+int('4 - 2')
+#Ошибка: ValueError: invalid literal for int() with base 10: '4 - 2'
+int('42')
+#можно
+int(-12.12)
+#можно
